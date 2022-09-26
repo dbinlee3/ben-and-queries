7e5980c383f55d535969429b5946acc8afefc24e
dbinlee3@gmail.com <dbinlee3@gmail.com>
2022-09-26 01:32:35 -0700

    started on education

--- src/pages/experience/education/Education.js
+++ src/pages/experience/education/Education.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { data } from "./EducationData.js";
 
 import styles from "./Education.module.css";
 
 function Education() {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h3>Education</h3>
+                <hr />
+            </div>
+
+            <ul className={styles.content}>
+                {data.map((item) => {
+                    return <li className={styles.entryContainer}>
+
+                    </li>   
+                })}
+            </ul>
+
         </div>
     )
 }
---
+++ src/pages/experience/education/EducationData.js
@@ -0,0 +1,16 @@
+export const data = [
+    {
+        title: "Cognitive Science: Design & Interaction",
+        school: "University of California, San Diego",
+        degree: "Bachelor's of Science",
+        completed: false,
+        date: "Spring 2023"
+    },
+    {
+        title: "Computer Science",
+        school: "University of California, San Diego",
+        degree: "Minor",
+        completed: false,
+        date: "Spring 2023"
+    }
+]
